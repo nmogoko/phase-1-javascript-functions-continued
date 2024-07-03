edbee652b8b536f1e86e7caeb9f884a83e6e0b08
nmogoko <+>
2024-07-03 17:47:18 +0100

    completed lab

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // code your solution here
-function saturdayFun (activity =`roller-skate`){
-    return `This Saturday, I want to ${activity}!`
+function saturdayFun (activity = 'roller-skate') {
+     return `This Saturday, I want to ${activity}!`;
 } 
+saturdayFun();
 saturdayFun(`bathe my dog`);
 
-const mondayWork = function (toDo = `go to the office`) {
-    return `This Monday, I will ${toDo}.`
-    } ;
-    mondayWork(`work from home`);
-
+function mondayWork (toDo = "go to the office") {
+   return `This Monday, I will ${toDo}.`;
+}
+mondayWork();
+mondayWork("take out trash");
 
 function wrapAdjective(accessory = "*"){
-   function(adjective = "special"){
-        return "You are ${accessory} ${adj}!"
+   return function(praise = "special"){
+        return `You are ${accessory}${praise}${accessory}!`;
     }
 }
-const encouragingPromptFunction = wrapAdjective("!!!");
-
+const encouragingPromptFunction = wrapAdjective()("loyal");
+console.log(encouragingPromptFunction);
